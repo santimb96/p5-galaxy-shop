138edd9aa00386d8c69478f94821af5ad7ae4fcd
Santiago <Martnez Bota>
2022-02-09 20:57:10 +0100

    front page acabado

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,14 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/shop">Shop</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <NavbarComponent />
   <router-view/>
   <FooterComponent />
 </template>
 
 <script>
 import FooterComponent from './components/FooterComponent'
+import NavbarComponent from './components/NavbarComponent'
 
 export default {
-  components: {FooterComponent},
-  render () {
-    return <FooterComponent />
-  }
+  components: {FooterComponent, NavbarComponent},
 }
 </script>
-
-<style>
-
-</style>
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-primary bg-primary">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/"><img src="../assets/logo.png" alt="logo"></router-link>
+      <button class="custom-toggler navbar-toggler" type="button" data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">HOME</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/shop">SHOP</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">ABOUT</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
