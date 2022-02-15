45c25048cb0cb2b946f326d327732c5320411507
Santiago <Martnez Bota>
2022-02-15 17:27:57 +0100

    carousel hecho y funcional

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,180 +1,246 @@
 <template>
-    <div class="container-fluid bg-dark">
-      <div class="row justify-content-center align-items-center">
-        <div class="card bg-transparent border-0 mt-3 satelite">
-          <img src="../assets/satellite.png" class="card-img-top" alt="satelite">
-          <div class="card-body">
-            <h1 class="card-title text-text text-center font-autowide">SOYUZ TMA-M</h1>
-            <h2 class="card-title text-text text-center">SPACECRAFT</h2>
-            <div class="col text-center">
-              <button class="btn btn-secondary regular-button">Read More</button>
+  <div class="container-fluid bg-dark">
+    <div class="row justify-content-center align-items-center">
+      <div class="card bg-transparent border-0 mt-3 satelite">
+        <img
+          src="../assets/satellite.png"
+          class="card-img-top"
+          alt="satelite"
+        />
+        <div class="card-body">
+          <h1 class="card-title text-text text-center font-autowide">
+            SOYUZ TMA-M
+          </h1>
+          <h2 class="card-title text-text text-center">SPACECRAFT</h2>
+          <div class="col text-center">
+            <button class="btn btn-secondary regular-button">Read More</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row justify-content-center">
+      <h1 class="text-text text-center font-autowide">FEATURED PROJECTS</h1>
+
+      <div
+        id="carouselExampleInterval"
+        class="carousel slide m-4"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-inner">
+          <div class="carousel-item active" data-bs-interval="10000">
+            <img
+              src="../assets/carousel/cohete-editado.jpg"
+              class="d-block w-100"
+              alt="space"
+            />
+          </div>
+          <div class="carousel-item" data-bs-interval="2000">
+            <img
+              src="../assets/carousel/paisaje-editado.jpg"
+              class="d-block w-100"
+              alt="space"
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="../assets/carousel/astronauta-editado.jpg"
+              class="d-block w-100"
+              alt="space"
+            />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleInterval"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleInterval"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  </div>
+
+  <!-- DIV SEPARADOR -->
+
+  <div class="mensaje bg-secondary">
+    <div class="contenido p-4">
+      <h1 class="titulo text-text text-center font-autowide">OUR MISSION</h1>
+      <p class="text-text text-center">
+        WE WANT TO CONQUER ANY LAND IN OUR WORLD, OR PASS THIS SUBJECT WITH HIGH
+        SCORE
+      </p>
+    </div>
+  </div>
+
+  <!-- MÁS CARTAS -->
+  <div class="bg-primary p-4 row">
+    <div class="container col-xl-6">
+      <h1 class="text-text text-center font-autowide">FEATURED VIDEO</h1>
+      <div class="ratio ratio-21x9 border border-text rounded">
+        <iframe src="https://www.youtube.com/embed/Y_plhk1FUQA"></iframe>
+      </div>
+    </div>
+    <div class="container col-xl-6">
+      <div class="row mt-3">
+        <div class="row-xl-6">
+          <div class="card bg-primary border-0">
+            <div class="card-body">
+              <div class="row">
+                <div
+                  class="col-xl-6 d-flex justify-content-center align-items-center"
+                >
+                  <img
+                    class="border border-text featured"
+                    src="../assets/rocket.jpg"
+                  />
+                </div>
+                <div class="col-xl-6">
+                  <h5 class="card-title text-text text-center m-2">
+                    DID YOU EXPECT SOMETHING?
+                  </h5>
+                  <p class="card-text text-text text-center">
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </p>
+                  <div class="boton text-center">
+                    <button
+                      type="button"
+                      class="btn btn-secondary"
+                      data-bs-container="body"
+                      data-bs-toggle="popover"
+                      data-bs-placement="bottom"
+                      data-bs-content="NO FA RES MÉS"
+                    >
+                      ¡CLICK ME!
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr class="hr" />
+        <div class="row-xl-6">
+          <div class="card bg-primary border-0">
+            <div class="card-body">
+              <div class="row">
+                <div
+                  class="col-xl-6 d-flex justify-content-center align-items-center"
+                >
+                  <img
+                    class="border border-text featured"
+                    src="../assets/rocket.jpg"
+                  />
+                </div>
+                <div class="col-xl-6">
+                  <h5 class="card-title text-text text-center m-2">
+                    YES OR NOT?
+                  </h5>
+                  <p class="card-text text-text text-center">
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </p>
+                  <div class="boton text-center">
+                    <button
+                      type="button"
+                      class="btn btn-secondary"
+                      data-bs-container="body"
+                      data-bs-toggle="popover"
+                      data-bs-placement="bottom"
+                      data-bs-content="NO FA RES MÉS"
+                    >
+                      ¡CLICK ME!
+                    </button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
+    </div>
+  </div>
 
+  <!-- REDES SOCIALES -->
 
+  <div class="social bg-rrss">
+    <div class="container text-white p-3">
       <div class="row text-center">
-        <h1 class="text-text text-center font-autowide">FEATURED PROJECTS</h1>
-        <div class="card bg-transparent border-0 col-sm-12 col-md-6 col-lg-6 col-xl-3">
-          <div class="card-body">
-            <img class="card-img-top border border-text projects"
-                 src="../assets/rocket.jpg" alt="rocket">
-          </div>
+        <div class="col-xl-6">
+          <h1 class="font-autowide">FOLLOW OUR MISSIONS ON</h1>
         </div>
 
-        <div class="card bg-transparent border-0 col-sm-12 col-md-6 col-lg-6 col-xl-3">
-          <div class="card-body">
-            <img class="card-img-top border border-text projects"
-                 src="../assets/planet.jpg" alt="planet">
-          </div>
-        </div>
-
-        <div class="card bg-transparent border-0 col-sm-12 col-md-6 col-lg-6 col-xl-3">
-          <div class="card-body">
-            <img class="card-img-top border border-text projects"
-                 src="../assets/space.jpg" alt="space">
-          </div>
-        </div>
-
-        <div class="card bg-transparent border-0 col-sm-12 col-md-6 col-lg-6 col-xl-3">
-          <div class="card-body">
-            <img class="card-img-top border border-text projects"
-                 src="../assets/moonRocket.jpg" alt="moon">
-          </div>
-        </div>
-      </div>
-
-    </div>
-
-    <!-- DIV SEPARADOR -->
-
-    <div class="mensaje bg-secondary">
-      <div class="contenido p-4">
-        <h1 class="titulo text-text text-center font-autowide">OUR MISSION</h1>
-        <p class="text-text text-center ">
-          WE WANT TO CONQUER ANY LAND IN OUR WORLD, OR PASS THIS SUBJECT WITH HIGH SCORE
-        </p>
-      </div>
-    </div>
-
-    <!-- MÁS CARTAS -->
-    <div class="bg-primary p-4 row">
-      <div class="container col-xl-6">
-        <h1 class="text-text text-center font-autowide">FEATURED VIDEO</h1>
-        <div class="ratio ratio-21x9 border border-text rounded">
-          <iframe src="https://www.youtube.com/embed/Y_plhk1FUQA"></iframe>
-        </div>
-      </div>
-      <div class="container col-xl-6">
-        <div class="row mt-3">
-          <div class="row-xl-6">
-            <div class="card bg-primary border-0">
-              <div class="card-body">
-                <div class="row">
-                  <div class="col-xl-6 d-flex justify-content-center align-items-center">
-                    <img class="border border-text featured"
-                         src="../assets/rocket.jpg"/>
-                  </div>
-                  <div class="col-xl-6">
-                    <h5 class="card-title text-text text-center m-2">DID YOU EXPECT SOMETHING?</h5>
-                    <p class="card-text text-text text-center">With supporting text below as a
-                      natural lead-in
-                      to
-                      additional content.</p>
-                    <div class="boton text-center">
-                      <button type="button" class="btn btn-secondary" data-bs-container="body"
-                              data-bs-toggle="popover" data-bs-placement="bottom"
-                              data-bs-content="NO FA RES MÉS">
-                        ¡CLICK ME!
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <hr class="hr">
-          <div class="row-xl-6">
-            <div class="card bg-primary border-0">
-              <div class="card-body">
-                <div class="row">
-                  <div class="col-xl-6 d-flex justify-content-center align-items-center">
-                    <img class="border border-text featured"
-                         src="../assets/rocket.jpg"/>
-                  </div>
-                  <div class="col-xl-6">
-                    <h5 class="card-title text-text text-center m-2">YES OR NOT?</h5>
-                    <p class="card-text text-text text-center">With supporting text below as a
-                      natural lead-in
-                      to
-                      additional content.</p>
-                    <div class="boton text-center">
-                      <button type="button" class="btn btn-secondary" data-bs-container="body"
-                              data-bs-toggle="popover" data-bs-placement="bottom"
-                              data-bs-content="NO FA RES MÉS">
-                        ¡CLICK ME!
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div class="col-xl-6 text-center">
+          <a href="http://freewebsitetemplates.com/go/facebook/" class="m-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              fill="white"
+              class="bi bi-facebook"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"
+              />
+            </svg>
+          </a>
+          <a href="http://freewebsitetemplates.com/go/twitter/" class="m-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              fill="white"
+              class="bi bi-twitter"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"
+              />
+            </svg>
+          </a>
+          <a href="http://freewebsitetemplates.com/go/googleplus/" class="m-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              fill="white"
+              class="bi bi-google"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"
+              />
+            </svg>
+          </a>
+          <a href="http://pinterest.com/fwtemplates/" class="m-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              fill="white"
+              class="bi bi-pinterest"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M8 0a8 8 0 0 0-2.915 15.452c-.07-.633-.134-1.606.027-2.297.146-.625.938-3.977.938-3.977s-.239-.479-.239-1.187c0-1.113.645-1.943 1.448-1.943.682 0 1.012.512 1.012 1.127 0 .686-.437 1.712-.663 2.663-.188.796.4 1.446 1.185 1.446 1.422 0 2.515-1.5 2.515-3.664 0-1.915-1.377-3.254-3.342-3.254-2.276 0-3.612 1.707-3.612 3.471 0 .688.265 1.425.595 1.826a.24.24 0 0 1 .056.23c-.061.252-.196.796-.222.907-.035.146-.116.177-.268.107-1-.465-1.624-1.926-1.624-3.1 0-2.523 1.834-4.84 5.286-4.84 2.775 0 4.932 1.977 4.932 4.62 0 2.757-1.739 4.976-4.151 4.976-.811 0-1.573-.421-1.834-.919l-.498 1.902c-.181.695-.669 1.566-.995 2.097A8 8 0 1 0 8 0z"
+              />
+            </svg>
+          </a>
         </div>
       </div>
     </div>
-
-    <!-- REDES SOCIALES -->
-
-    <div class="social bg-rrss">
-      <div class="container text-white p-3">
-        <div class="row text-center">
-          <div class="col-xl-6">
-            <h1 class="font-autowide">FOLLOW OUR MISSIONS ON</h1>
-          </div>
-
-          <div class="col-xl-6 text-center">
-
-
-            <a href="http://freewebsitetemplates.com/go/facebook/" class="m-3">
-              <svg
-                  xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="white"
-                  class="bi bi-facebook" viewBox="0 0 16 16">
-                <path
-                    d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
-              </svg>
-            </a>
-            <a href="http://freewebsitetemplates.com/go/twitter/" class="m-3">
-              <svg
-                  xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="white" class="bi bi-twitter"
-                  viewBox="0 0 16 16">
-                <path
-                    d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
-              </svg>
-            </a>
-            <a href="http://freewebsitetemplates.com/go/googleplus/" class="m-3">
-              <svg
-                  xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="white" class="bi bi-google"
-                  viewBox="0 0 16 16">
-                <path
-                    d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"/>
-              </svg>
-            </a>
-            <a href="http://pinterest.com/fwtemplates/" class="m-3">
-              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="white"
-                   class="bi bi-pinterest" viewBox="0 0 16 16">
-                <path
-                    d="M8 0a8 8 0 0 0-2.915 15.452c-.07-.633-.134-1.606.027-2.297.146-.625.938-3.977.938-3.977s-.239-.479-.239-1.187c0-1.113.645-1.943 1.448-1.943.682 0 1.012.512 1.012 1.127 0 .686-.437 1.712-.663 2.663-.188.796.4 1.446 1.185 1.446 1.422 0 2.515-1.5 2.515-3.664 0-1.915-1.377-3.254-3.342-3.254-2.276 0-3.612 1.707-3.612 3.471 0 .688.265 1.425.595 1.826a.24.24 0 0 1 .056.23c-.061.252-.196.796-.222.907-.035.146-.116.177-.268.107-1-.465-1.624-1.926-1.624-3.1 0-2.523 1.834-4.84 5.286-4.84 2.775 0 4.932 1.977 4.932 4.62 0 2.757-1.739 4.976-4.151 4.976-.811 0-1.573-.421-1.834-.919l-.498 1.902c-.181.695-.669 1.566-.995 2.097A8 8 0 1 0 8 0z"/>
-              </svg>
-            </a>
-
-          </div>
-        </div>
-      </div>
-    </div>
+  </div>
 </template>
-
-<style>
-
-</style>
