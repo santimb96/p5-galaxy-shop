51768c5cae80c10a2fa10c518db208e440dd1c10
Santiago <Martnez Bota>
2022-02-15 21:06:42 +0100

    shopping hecha (60%)

--- src/components/ShopComponent.vue
+++ src/components/ShopComponent.vue
@@ -1,13 +1,179 @@
 <template>
-  <dir>Hi!</dir>
+  <div class="container-fluid bg-dark">
+    <div class="row">
+      <div class="col-lg-10">
+        <div class="row">
+          <div
+            class="col-12 col-sm-6 col-md-4 d-flex justify-content-center mb-2"
+          >
+            <div class="card bg-secondary text-white" style="width: 20rem">
+              <img
+                src="../assets/rocketLeague.webp"
+                class="img-fluid rounded m-3"
+                alt="space"
+              />
+              <div class="card-body">
+                <h1 class="card-title text-center">Viaje a la Luna</h1>
+                <h5 class="text-center">19,95€</h5>
+                <p class="card-text text-center">
+                  Viaje a la luna por parte de adminerTravel
+                </p>
+                <div class="text-center">
+                  <a href="#" class="btn btn-text producto">Comprar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            class="col-12 col-sm-6 col-md-4 d-flex justify-content-center mb-2"
+          >
+            <div class="card bg-secondary text-white" style="width: 20rem">
+              <img
+                src="../assets/rocketLeague.webp"
+                class="img-fluid rounded m-3"
+                alt="space"
+              />
+              <div class="card-body">
+                <h1 class="card-title text-center">Viaje a la Luna</h1>
+                <h5 class="text-center">19,95€</h5>
+                <p class="card-text text-center">
+                  Viaje a la luna por parte de adminerTravel
+                </p>
+                <div class="text-center">
+                  <a href="#" class="btn btn-text producto">Comprar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="col-12 col-sm-6 col-md-4 d-flex justify-content-center mb-2"
+          >
+            <div class="card bg-secondary text-white" style="width: 20rem">
+              <img
+                src="../assets/rocketLeague.webp"
+                class="img-fluid rounded m-3"
+                alt="space"
+              />
+              <div class="card-body">
+                <h1 class="card-title text-center">Viaje a la Luna</h1>
+                <h5 class="text-center">19,95€</h5>
+                <p class="card-text text-center">
+                  Viaje a la luna por parte de adminerTravel
+                </p>
+                <div class="text-center">
+                  <a href="#" class="btn btn-text producto">Comprar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="col-12 col-sm-6 col-md-4 d-flex justify-content-center mb-2"
+          >
+            <div class="card bg-secondary text-white" style="width: 20rem">
+              <img
+                src="../assets/rocketLeague.webp"
+                class="img-fluid rounded m-3"
+                alt="space"
+              />
+              <div class="card-body">
+                <h1 class="card-title text-center">Viaje a la Luna</h1>
+                <h5 class="text-center">19,95€</h5>
+                <p class="card-text text-center">
+                  Viaje a la luna por parte de adminerTravel
+                </p>
+                <div class="text-center">
+                  <a href="#" class="btn btn-text producto">Comprar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-2">
+        <div class="card bg-text" style="height: 100%">
+          <div class="row">
+            <div class="col-12 d-flex justify-content-center">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-4">
+                    <p class="card-title">Luna</p>
+                  </div>
+                  <div class="col-4">
+                    <p class="card-title"><small>19,95€</small></p>
+                  </div>
+                  <div class="col-4">
+                    <input
+                      type="number"
+                      name="unidad"
+                      value="1"
+                      style="width: 3rem"
+                    />
+                  </div>
+                </div>
+
+
+              </div>
+            </div>
+
+            <div class="col-12 d-flex justify-content-center">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-4">
+                    <p class="card-title">Luna</p>
+                  </div>
+                  <div class="col-4">
+                    <p class="card-title"><small>19,95€</small></p>
+                  </div>
+                  <div class="col-4">
+                    <input
+                      type="number"
+                      name="unidad"
+                      value="1"
+                      style="width: 3rem"
+                    />
+                  </div>
+                </div>
+
+
+              </div>
+            </div>
+
+            <div class="col-12 d-flex justify-content-center">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-4">
+                    <p class="card-title">Luna</p>
+                  </div>
+                  <div class="col-4">
+                    <p class="card-title"><small>19,95€</small></p>
+                  </div>
+                  <div class="col-4">
+                    <input
+                      type="number"
+                      name="unidad"
+                      value="1"
+                      style="width: 3rem"
+                    />
+                  </div>
+                </div>
+
+                <!-- <p class="text-center">19,95€</p> -->
+                <div class="text-center">
+                  <a href="#" class="btn btn-secondary producto">Comprar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
